10189b1da21d09d2ae9ceef43c606afb0a456e1b
Joel Ansbro <joelmatthewansbro@gmail.com>
2021-09-09 13:49:40 +0100

    form build

--- src/App.js
+++ src/App.js
@@ -1,19 +1,87 @@
-import React, { Component } from 'react';
-import Palette from './Palette';
-import seedColors from './seedColors';
-
-
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
-class App extends Component{
-  render(){
-    return(
-      <div>
-        <Palette palette={seedColors[5]}/>
-      </div>
-
-    )
+  const formReducer = (state, event)=>{
+    if(event.reset) {
+      return {
+        apple: '',
+        count: 0,
+        name: '',
+        'gift-wrap': false,
+      }
+    }
+    return {
+      ...state,
+      [event.name]: event.value
+    }
   }
+
+function App() {
+  const [formData, setFormData] = useReducer(formReducer, {
+    count: 100,
+  });
+  const [submitting, setSubmitting ] = useState(false);
+
+  const handleSubmit = event => {
+   event.preventDefault();
+   setSubmitting(true);
+
+   setTimeout(() => {
+     setSubmitting(false);
+   }, 3000)
+ }
+
+ const handleChange = event =>{
+   const isCheckbox = event.target.type === 'checkbox';
+   setFormData({
+     name: event.target.name,
+     value: isCheckbox ? event.target.checked : event.target.value,
+   });
+ }
+  return (
+    <div className="wrapper">
+      <h1>How About Them Apples</h1>
+      {submitting &&
+      <div>
+        You are submitting the following:
+        <ul>
+          {Object.entries(formData).map(([name, value]) => (
+            <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+          ))}
+        </ul>
+      </div>
+      }
+
+      <form onSubmit={handleSubmit}>
+      <fieldset>
+         <label>
+           <p>Name</p>
+           <input name="name" onChange={handleChange} value={formData.name || ''}/>
+         </label>
+       </fieldset>
+       <fieldset>
+         <label>
+           <p>Apples</p>
+           <select name="apple" onChange={handleChange} value={formData.apple || ''}>
+               <option value="">--Please choose an option--</option>
+               <option value="fuji">Fuji</option>
+               <option value="jonathan">Jonathan</option>
+               <option value="honey-crisp">Honey Crisp</option>
+           </select>
+         </label>
+         <label>
+           <p>Count</p>
+           <input type="number" name="count" onChange={handleChange} step="1" value={formData.count || ''}/>
+         </label>
+         <label>
+           <p>Gift Wrap</p>
+           <input type="checkbox" name="gift-wrap" onChange={handleChange} checked={formData['gift-wrap'] || false} />
+         </label>
+       </fieldset>
+       <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default App;
