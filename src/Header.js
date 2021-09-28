a0b675e3ce1287176c5d2fcded513d59161e7f4c
Joel Ansbro <joelmatthewansbro@gmail.com>
2021-09-28 16:41:45 +0100

    changed styling

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './App.css';
 import './bulma.min.css';
+import './App.css';
 
 function Header(){
     return(
         <nav className="navbar is-dark">
             <div className="navbar-item">
-                Github
+                <a href="https://github.com/joelansbro">Github</a>
             </div>
             <div className="navbar-item">
-                Twitter
+                <a href="https://twitter.com/joel_ansbro">Twitter</a>
             </div>
             <div className="navbar-item">
-                Blog
+                <a href="https://joelansbro.com">Blog</a>
             </div>
         </nav>
     )
--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Header from './Header';
-import Footer from './Footer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
     <App />
-    <Footer />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
