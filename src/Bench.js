b3dc39e1b0e6377e026b8e6b8ff1e43599938011
Joel Ansbro <joelmatthewansbro@gmail.com>
2021-12-08 12:56:49 +0000

    updated bench.js
    updated the calculation data for bench.js so that it is inline with the strength ratios

--- src/Bench.js
+++ src/Bench.js
@@ -16,29 +16,16 @@ const formReducer = (state, event)=>{
   }
 
   const ratios = {
-    'frontSquat': 0.85,
-    'cleanDeadlift': 1,
-    'snatchDeadlift': 0.9,
-    'powerDeadlift': 1.2,
-    'benchPress': 0.75,
-    'closeGripBenchPress': 0.675,
-    'pushPress': 0.6375,
-    'inclineBenchPress': 0.6,
-    'militaryPress': 0.45,
-    'weightedDip': 0.7875,
-    'supinatedChinUp': 0.675,
-    'chestSupportedBarbellRow': 0.525,
-    'preacherCurl': 0.3,
-    'standingReverseCurl': 0.2625,
-    'cleanAndJerk': 0.8,
-    'snatch': 0.66,
-    'clean': 0.816,
-    'jerk': 0.84,
-    'powerClean': 0.68,
-    'powerJerk': 0.72,
-    'powerSnatch': 0.54,
+    'closegripbenchpress': 0.9,
+    'pushpress': 0.85,
+    'inclinebench': 0.8,
+    'militarypress': 0.6,
+    'weighteddip': 1.05,
+    'supinatedchinup': 0.9,
+    'chestsupportedbarbellrow': 0.7,
+    'preachercurl': 0.4,
+    'standingreversecurl': 0.35
     }
-
 
     function Bench() {
         const [formData, setFormData] = useReducer(formReducer, {
@@ -79,162 +66,71 @@ const formReducer = (state, event)=>{
       <div className="tile is-ancestor">
         <div className="tile is-parent">
           <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Front Squat</p>
-            
-            <p className="subtitle">{(ratios.frontSquat*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Clean Deadlift</p>
-            
-            <p className="subtitle">{(ratios.cleanDeadlift*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Snatch Deadlift</p>
-            
-            <p className="subtitle">{(ratios.snatchDeadlift*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Power Deadlift</p>
-            
-            <p className="subtitle">{(ratios.powerDeadlift*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-      </div>
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Bench Press</p>
-            
-            <p className="subtitle">{(ratios.benchPress*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
             <p className="subtitle">Close-Grip Bench Press</p>
             
-            <p className="subtitle">{(ratios.closeGripBenchPress*formData.count).toFixed()}kg</p>
+            <p className="subtitle">{(ratios.closegripbenchpress*formData.count).toFixed()}kg</p>
           </article>
         </div>
         <div className="tile is-parent">
           <article className="tile is-child notification is-dark has-text-centered">
             <p className="subtitle">Push Press</p>
             
-            <p className="subtitle">{(ratios.pushPress*formData.count).toFixed()}kg</p>
+            <p className="subtitle">{(ratios.pushpress*formData.count).toFixed()}kg</p>
           </article>
         </div>
         <div className="tile is-parent">
           <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Incline Bench Press</p>
+            <p className="subtitle">Incline Bench</p>
             
-            <p className="subtitle">{(ratios.inclineBenchPress*formData.count).toFixed()}kg</p>
+            <p className="subtitle">{(ratios.inclinebench*formData.count).toFixed()}kg</p>
           </article>
         </div>
-      </div>
-      <div className="tile is-ancestor">
         <div className="tile is-parent">
           <article className="tile is-child notification is-dark has-text-centered">
             <p className="subtitle">Military Press</p>
             
-            <p className="subtitle">{(ratios.militaryPress*formData.count).toFixed()}kg</p>
+            <p className="subtitle">{(ratios.militarypress*formData.count).toFixed()}kg</p>
           </article>
         </div>
+      </div>
+      <div className="tile is-ancestor">
         <div className="tile is-parent">
           <article className="tile is-child notification is-dark has-text-centered">
             <p className="subtitle">Weighted Dip</p>
             
-            <p className="subtitle">{(ratios.weightedDip*formData.count).toFixed()}kg</p>
+            <p className="subtitle">{(ratios.weighteddip*formData.count).toFixed()}kg</p>
           </article>
         </div>
         <div className="tile is-parent">
           <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Supinated Chin Up</p>
+            <p className="subtitle">Supinated Chin-Up</p>
             
-            <p className="subtitle">{(ratios.supinatedChinUp*formData.count).toFixed()}kg</p>
+            <p className="subtitle">{(ratios.supinatedchinup*formData.count).toFixed()}kg</p>
           </article>
         </div>
         <div className="tile is-parent">
           <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Chest Supported Barbell Row</p>
+            <p className="subtitle">Chest-Supported Barbell Row</p>
             
-            <p className="subtitle">{(ratios.chestSupportedBarbellRow*formData.count).toFixed()}kg</p>
+            <p className="subtitle">{(ratios.chestsupportedbarbellrow*formData.count).toFixed()}kg</p>
           </article>
         </div>
-      </div>
-      <div className="tile is-ancestor">
         <div className="tile is-parent">
           <article className="tile is-child notification is-dark has-text-centered">
             <p className="subtitle">Preacher Curl</p>
             
-            <p className="subtitle">{(ratios.preacherCurl*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Standing Reverse Curl</p>
-            
-            <p className="subtitle">{(ratios.standingReverseCurl*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Clean and Jerk</p>
-            
-            <p className="subtitle">{(ratios.cleanAndJerk*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Snatch</p>
-            
-            <p className="subtitle">{(ratios.snatch*formData.count).toFixed()}kg</p>
+            <p className="subtitle">{(ratios.preachercurl*formData.count).toFixed()}kg</p>
           </article>
         </div>
       </div>
       <div className="tile is-ancestor">
         <div className="tile is-parent">
           <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Clean</p>
-            
-            <p className="subtitle">{(ratios.clean*formData.count).toFixed()}kg</p>
+            <p className="subtitle">Standing Reverse-Curl</p>
+            <p className="subtitle">{(ratios.standingreversecurl*formData.count).toFixed()}kg</p>
           </article>
         </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Jerk</p>
-            
-            <p className="subtitle">{(ratios.jerk*formData.count).toFixed()}kg</p>
-          </article>
         </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Power Clean</p>
-            
-            <p className="subtitle">{(ratios.powerClean*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Power Jerk</p>
-            
-            <p className="subtitle">{(ratios.powerJerk*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-      </div>
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-dark has-text-centered">
-            <p className="subtitle">Power Snatch</p>
-            
-            <p className="subtitle">{(ratios.powerSnatch*formData.count).toFixed()}kg</p>
-          </article>
-        </div>
-      </div>
       </div>
       }
           </div>
